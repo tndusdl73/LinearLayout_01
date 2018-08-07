1cf5cc74d9b3a2e5b2fa5f944ef0bf6e6e784d5b
tndusdl73 <tndusdl73@naver.com>
2018-08-07 15:27:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tndus/linearlayout_01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tndus.linearlayout_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.LinearLayout02);
+    }
+}
